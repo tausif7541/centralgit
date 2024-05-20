a15b5195c19529860a9be551852889d6d1eaa251
tausif7541 <138056156+tausif7541@users.noreply.github.com>
2024-05-21 02:31:17 +0530

    Update MainController.java

--- src/main/java/com/example/SpringDocker/SpringDocker/MainController.java
+++ src/main/java/com/example/SpringDocker/SpringDocker/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 	@RestController
 	public class MainController {
 
-		@RequestMapping("/")
+		@RequestMapping("/hello")
 		public String hello() {
 			return "Hello World";
 
